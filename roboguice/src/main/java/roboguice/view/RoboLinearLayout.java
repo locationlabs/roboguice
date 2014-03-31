a64e654b0496d9648f74732928ddc60c55693531
Brian Martin <brian@locationlabs.com>
2014-03-31 09:06:34 -0700

    add RoboLinearLayout, RoboRelativeLayout, RoboScrollView, and RoboView

---
+++ roboguice/src/main/java/roboguice/view/RoboLinearLayout.java
@@ -0,0 +1,96 @@
+package roboguice.view;
+
+import roboguice.RoboGuice;
+import roboguice.activity.event.*;
+import roboguice.event.EventManager;
+import roboguice.inject.ContentViewListener;
+import roboguice.inject.RoboInjector;
+import roboguice.util.RoboContext;
+import android.util.Log;
+import android.util.AttributeSet;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.content.Context;
+
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import com.google.inject.Key;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A {@link RoboActivity} extends from {@link Activity} to provide dynamic
+ * injection of collaborators, using Google Guice.<br />
+ * <br />
+ * Your own activities that usually extend from {@link Activity} should now
+ * extend from {@link RoboActivity}.<br />
+ * <br />
+ * If your activities extend from subclasses of {@link Activity} provided by the
+ * Android SDK, we provided Guice versions as well for the most used : see
+ * {@link RoboExpandableListActivity}, {@link RoboListActivity}, and other
+ * classes located in package <strong>roboguice.activity</strong>.<br />
+ * <br />
+ * If we didn't provide what you need, you have two options : either post an
+ * issue on <a href="http://code.google.com/p/roboguice/issues/list">the bug
+ * tracker</a>, or implement it yourself. Have a look at the source code of this
+ * class ({@link RoboActivity}), you won't have to write that much changes. And
+ * of course, you are welcome to contribute and send your implementations to the
+ * RoboGuice project.<br />
+ * <br />
+ * Please be aware that collaborators are not injected into this until you call
+ * {@link #setContentView(int)} (calling any of the overloads of this methods
+ * will work).<br />
+ * <br />
+ * You can have access to the Guice {@link Injector} at any time, by calling
+ * {@link #getInjector()}.<br />
+ * However, you will not have access to ContextSingleton scoped beans until
+ * {@link #onCreate(Bundle)} is called. <br />
+ * <br />
+ * 
+ */
+public class RoboLinearLayout extends android.widget.LinearLayout implements RoboContext {
+   protected EventManager eventManager;
+   protected HashMap<Key<?>,Object> scopedObjects = new HashMap<Key<?>, Object>();
+
+   @Inject ContentViewListener ignored; // BUG find a better place to put this
+
+   public RoboLinearLayout (Context context) {
+      super(context);
+      init(context);
+   }
+
+   public RoboLinearLayout (Context context, AttributeSet attrs) {
+      super(context, attrs);
+      init(context);
+   }
+   /*
+   public RoboLinearLayout (Context context, AttributeSet attrs, int defStyle) {
+      super(context, attrs, defStyle);
+      init(context);
+   }
+   */
+   private void init(Context context) {
+      Log.d("XXX", "RoboLinearLayout init");
+      final RoboInjector injector = RoboGuice.getInjector(getContext());
+      eventManager = injector.getInstance(EventManager.class);
+      injector.injectMembersWithoutViews(this);
+      //eventManager.fire(new OnCreateEvent(savedInstanceState));
+   }
+
+   @Override
+   protected void onFinishInflate() {
+      super.onFinishInflate();
+      Log.d("XXX", "onFinishInflate");
+      RoboGuice.getInjector(getContext()).injectViewMembers(this);
+      //eventManager.fire(new OnContentChangedEvent());
+   }
+
+   @Override
+   public Map<Key<?>, Object> getScopedObjectMap() {
+      return scopedObjects;
+   }
+}
---
+++ roboguice/src/main/java/roboguice/view/RoboView.java
@@ -0,0 +1,98 @@
+package roboguice.view;
+
+import roboguice.RoboGuice;
+import roboguice.activity.event.*;
+import roboguice.event.EventManager;
+import roboguice.inject.ContentViewListener;
+import roboguice.inject.RoboInjector;
+import roboguice.util.RoboContext;
+import android.util.Log;
+import android.util.AttributeSet;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.os.Bundle;
+
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+import com.google.inject.Key;
+
+import java.util.HashMap;
+import java.util.Map;
+import android.view.View;
+import android.content.Context;
+
+/**
+ * A {@link RoboActivity} extends from {@link Activity} to provide dynamic
+ * injection of collaborators, using Google Guice.<br />
+ * <br />
+ * Your own activities that usually extend from {@link Activity} should now
+ * extend from {@link RoboActivity}.<br />
+ * <br />
+ * If your activities extend from subclasses of {@link Activity} provided by the
+ * Android SDK, we provided Guice versions as well for the most used : see
+ * {@link RoboExpandableListActivity}, {@link RoboListActivity}, and other
+ * classes located in package <strong>roboguice.activity</strong>.<br />
+ * <br />
+ * If we didn't provide what you need, you have two options : either post an
+ * issue on <a href="http://code.google.com/p/roboguice/issues/list">the bug
+ * tracker</a>, or implement it yourself. Have a look at the source code of this
+ * class ({@link RoboActivity}), you won't have to write that much changes. And
+ * of course, you are welcome to contribute and send your implementations to the
+ * RoboGuice project.<br />
+ * <br />
+ * Please be aware that collaborators are not injected into this until you call
+ * {@link #setContentView(int)} (calling any of the overloads of this methods
+ * will work).<br />
+ * <br />
+ * You can have access to the Guice {@link Injector} at any time, by calling
+ * {@link #getInjector()}.<br />
+ * However, you will not have access to ContextSingleton scoped beans until
+ * {@link #onCreate(Bundle)} is called. <br />
+ * <br />
+ * 
+ */
+public class RoboView extends View implements RoboContext {
+   protected EventManager eventManager;
+   protected HashMap<Key<?>,Object> scopedObjects = new HashMap<Key<?>, Object>();
+
+   @Inject ContentViewListener ignored; // BUG find a better place to put this
+
+   public RoboView (Context context) {
+      super(context);
+      init(context);
+   }
+
+   public RoboView (Context context, AttributeSet attrs) {
+      super(context, attrs);
+      init(context);
+   }
+
+
+   public RoboView (Context context, AttributeSet attrs, int defStyle) {
+      super(context, attrs, defStyle);
+      init(context);
+   }
+
+   private void init(Context context) {
+      Log.d("XXX", "RoboView init");
+      final RoboInjector injector = RoboGuice.getInjector(getContext());
+      eventManager = injector.getInstance(EventManager.class);
+      injector.injectMembersWithoutViews(this);
+      //eventManager.fire(new OnCreateEvent(savedInstanceState));
+   }
+
+   @Override
+   protected void onFinishInflate() {
+      super.onFinishInflate();
+      Log.d("XXX", "onFinishInlfate");
+      RoboGuice.getInjector(getContext()).injectViewMembers(this);
+      eventManager.fire(new OnContentChangedEvent());
+   }
+
+   @Override
+   public Map<Key<?>, Object> getScopedObjectMap() {
+      return scopedObjects;
+   }
+}
